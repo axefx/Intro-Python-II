83e838781643567095ae5928fe39ad9a727954a1
Axel <axacorro@gmail.com>
2020-06-04 13:15:45 -0700

    removed extra methods and attributes

--- src/player.py
+++ src/player.py
@@ -1,14 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
-        self.name = name
-        self.current_room = current_room
-        self.room_name = self.current_room.get_name()
-
-    def get_currentroom(self):
-        return self.current_room
-
-    def update_currentroom(self, room):
-        self.current_room = room
-        return self.get_currentroom()
+    def __init__(self, location):
+        self.location = location
+        
