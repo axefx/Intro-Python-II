e1b816454795cf4dbcdb2bdb87a42f7124ea6d8e
Axel <axacorro@gmail.com>
2020-06-04 13:13:53 -0700

    changed get_name method to __str__ method

--- src/room.py
+++ src/room.py
@@ -5,5 +5,5 @@ class Room:
         self.name = name
         self.description = description
 
-    def get_name(self):
-        return self.name
+    def __str__(self):
+        return f"{self.name}: {self.description}"
